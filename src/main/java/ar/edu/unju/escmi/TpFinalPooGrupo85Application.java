3d0ef938a2be218932c3f919cc5270f34ddaee1b
ENJ23 <eliasjarma9@gmail.com>
2024-11-13 19:43:15 -0300

    Inicializacion Proyecto FInal POO

---
+++ src/main/java/ar/edu/unju/escmi/TpFinalPooGrupo85Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.escmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalPooGrupo85Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpFinalPooGrupo85Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unju/escmi/TpFinalPooGrupo85ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.escmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFinalPooGrupo85ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
