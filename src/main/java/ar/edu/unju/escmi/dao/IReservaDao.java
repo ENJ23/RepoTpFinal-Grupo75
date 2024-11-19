16965e697f37df24c4b8d4faeb7e880eb076c1ba
Sergio <PC@Sertin468>
2024-11-18 21:28:18 -0300

    Finalizacion de la Clase IReservaDao

--- src/main/java/ar/edu/unju/escmi/dao/IReservaDao.java
+++ src/main/java/ar/edu/unju/escmi/dao/IReservaDao.java
@@ -1,5 +1,14 @@
 package ar.edu.unju.escmi.dao;
 
-public class IReservaDao {
+import java.util.List;
 
+import ar.edu.unju.escmi.dominio.Reserva;
+
+public interface IReservaDao {
+	
+	public void realizarReserva(Reserva reserva);
+	public void pagarReserva(Reserva reserva, double pagoNuevo);
+	public void consultarReservas();
+	public List<Reserva> listarReservas();
+	public Reserva buscarReserva(long id);
 }
