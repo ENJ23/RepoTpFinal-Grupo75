2e82ff98beb70eceb7e531030241dfa9ab136d16
Sergio <PC@Sertin468>
2024-11-18 21:24:11 -0300

    Finalizacion clase IClienteDao

--- src/main/java/ar/edu/unju/escmi/dao/IClienteDao.java
+++ src/main/java/ar/edu/unju/escmi/dao/IClienteDao.java
@@ -1,5 +1,13 @@
 package ar.edu.unju.escmi.dao;
 
-public class IClienteDao {
- 
+import ar.edu.unju.escmi.dominio.Cliente;
+import ar.edu.unju.escmi.exceptions.ClienteNoRegistradoException;
+
+public interface IClienteDao {
+
+	public void guardarCliente(Cliente cliente);
+	public void modificarCliente(Cliente clienteAModificar, Cliente datosNuevos);
+	public Cliente buscarCliente(long id) throws ClienteNoRegistradoException;
+	public void consultarClientes();
+	public void eliminarCliente(Cliente cliente);
 }
